b2b49ac456b1336d04f519eb019de639c5bf03c4
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-10-06 12:53:46 +0200

    Add upload thumbnail funcionality

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,48 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20 // 10 MB
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	r.ParseMultipartForm(maxMemory)
+
+	fileData, fileHeaders, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, 500, "Couldn't get a fileData", err)
+	}
+
+	mediaType := fileHeaders.Header.Get("Content-Type")
+
+	thumbnailData, err := io.ReadAll(fileData)
+	if err != nil {
+		respondWithError(w, 500, "Couldn't get a data of the image", err)
+
+	}
+
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, 500, "Error of the getting videoMetadata from the database", err)
+
+	}
+	if videoMetadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "The video is not the user property", err)
+	}
+	thb := thumbnail{
+		data:      thumbnailData,
+		mediaType: mediaType,
+	}
+
+	videoThumbnails[videoMetadata.ID] = thb
+
+	newThbUrl := fmt.Sprintf("http://localhost:%v/api/thumbnails/%v", cfg.port, videoMetadata.ID)
+
+	videoMetadata.ThumbnailURL = &newThbUrl
+	err = cfg.db.UpdateVideo(videoMetadata)
+	if err != nil {
+		respondWithError(w, 500, "There was an error while updating the video in the database", err)
+	}
+
+	respondWithJSON(w, http.StatusOK, videoMetadata)
 }
